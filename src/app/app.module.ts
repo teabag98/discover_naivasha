d0ac14a9c053d8f255aab10cc088b2274973c915
teabag98 <eric.kingoo2014@gmail.com>
2022-08-10 15:01:31 +0300

    add experiences cards and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BodyComponent } from './body/body.component';
+import { DestinationsComponent } from './destinations/destinations.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent, 
+    NavbarComponent,
+    BodyComponent,
+    DestinationsComponent,
+    ExperiencesComponent, 
   ],
   imports: [
     BrowserModule,
